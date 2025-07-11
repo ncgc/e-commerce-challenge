dc0648d6c9899b81a63ba046608174717aa3b8d1
Natalia <natalia.datascience@gmail.com>
2025-07-11 19:32:31 -0300

    feat: add cart item as a reference between a cart and products

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,6 @@
 class Cart < ApplicationRecord
+  has_many :cart_items, dependent: :destroy
+
   validates_numericality_of :total_price, greater_than_or_equal_to: 0
 
   # TODO: lógica para marcar o carrinho como abandonado e remover se abandonado
