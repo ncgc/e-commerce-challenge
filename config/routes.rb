555846e2385e5ebaa1f1dcab45a6bb92d5866064
Natalia <natalia.datascience@gmail.com>
2025-07-12 01:37:37 -0300

    feat: add route to add product in cart

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
   resources :products
+  
+  post '/cart', to: 'carts#add_product'
+
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "rails/health#show"
