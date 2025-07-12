3696196112dce65efc1bc75e11a922255aa053cd
Natalia <natalia.datascience@gmail.com>
2025-07-12 01:41:50 -0300

    feat: calculate total price per item and total price per cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,7 +1,10 @@
 class Cart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
+  has_many :products, through: :cart_items
 
   validates_numericality_of :total_price, greater_than_or_equal_to: 0
 
-  # TODO: lógica para marcar o carrinho como abandonado e remover se abandonado
+  def total_price
+    cart_items.sum(&:total_price) || 0
+  end
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,11 @@
 class CartItem < ApplicationRecord
   belongs_to :product
   belongs_to :cart
+
+  validates :quantity, presence: true, numericality: { greater_than: 0 }
+  validates :product_id, uniqueness: { scope: :cart_id, message: "already in cart" }
+
+  def total_price
+    quantity * product.price
+  end
 end
